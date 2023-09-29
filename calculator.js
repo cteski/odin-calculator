1ffe3018de8f4b83706813a893699b33e5be2c0a
Chris Teski <12370776+cteski@users.noreply.github.com>
2023-09-29 00:01:40 -0400

    Add functionality to display clicked button values

--- calculator.js
+++ calculator.js
@@ -2,6 +2,17 @@ let numA = 0;
 let numB = 0;
 let operator = '';
 
+const calculatorDiv = document.querySelector('#calculator');
+const displayDiv = calculatorDiv.querySelector('#display');
+const buttons = calculatorDiv.querySelectorAll('button');
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        displayDiv.textContent += button.textContent;
+    });
+})
+
 function add(x, y) {
     return x + y;
 }
