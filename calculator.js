4f96f702d352609a2c00dce4a7b9c7cd4f64abaa
Chris Teski <12370776+cteski@users.noreply.github.com>
2023-09-28 23:42:38 -0400

    Add operate function with basic functionality

--- calculator.js
+++ calculator.js
@@ -17,3 +17,23 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(x, y, operator) {
+    let result = 0;
+    switch(operator) {
+        case '+':
+            result = add(x, y);
+            break;
+        case '-':
+            result = subtract(x, y);
+            break;
+        case '*':
+            result = multiply(x, y);
+            break;
+        case '/':
+            result = divide(x, y);
+            break;
+    }
+
+    return result;
+}
